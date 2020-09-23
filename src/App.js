f3eadc2a1472538f4ac3f8b8b88ec946f08ed929
Peter <Posoo617@gmail.com>
2020-09-23 15:44:13 +0300

    Create Pages

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react"
 import "./App.css"
 import Navbar from "./components/Navbar"
 import { Switch, Route } from "react-router-dom"
+import Home from "./pages/Home"
 
 const App = () => {
 	return (
 		<React.Fragment>
 			<Navbar />
 			<Switch>
-				<Route path="/" exact />
+				<Route path="/" exact component={Home} />
 			</Switch>
 		</React.Fragment>
 	)
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "../App.css"
+import Hero from "../components/Hero"
+
+const Home = () => {
+	return (
+		<>
+			<Hero />
+			
+		</>
+	)
+}
+
+export default Home
