444e42efcd79262985f38b42dbce6d058a6978a4
Peter <Posoo617@gmail.com>
2020-09-25 17:03:42 +0300

    Footer Component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,14 @@ import React from "react"
 import "../App.css"
 import Hero from "../components/Hero"
 import Cards from "../components/Cards"
+import Footer from "../components/Footer"
 
 const Home = () => {
 	return (
 		<>
 			<Hero />
 			<Cards />
+			<Footer />
 		</>
 	)
 }
