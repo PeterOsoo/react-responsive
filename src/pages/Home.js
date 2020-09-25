dfe538ba8ddb0d33fa62c4d2d34cbad9eb5de487
Peter <Posoo617@gmail.com>
2020-09-25 16:40:31 +0300

    Create Card Components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
 import React from "react"
 import "../App.css"
 import Hero from "../components/Hero"
+import Cards from "../components/Cards"
 
 const Home = () => {
 	return (
 		<>
 			<Hero />
-			
+			<Cards />
 		</>
 	)
 }
