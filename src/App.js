b7e55bf8176613f4bd333328b6e243ae6c2a7a25
Peter <Posoo617@gmail.com>
2020-09-25 16:41:03 +0300

    Create more Pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import "./App.css"
 import Navbar from "./components/Navbar"
 import { Switch, Route } from "react-router-dom"
 import Home from "./pages/Home"
+import Services from "./pages/Services"
+import Products from "./pages/Products"
+import SignUp from "./pages/SignUp"
 
 const App = () => {
 	return (
@@ -10,6 +13,9 @@ const App = () => {
 			<Navbar />
 			<Switch>
 				<Route path="/" exact component={Home} />
+				<Route path="/services" component={Services} />
+				<Route path="/products" component={Products} />
+				<Route path="/sign-up" component={SignUp} />
 			</Switch>
 		</React.Fragment>
 	)
